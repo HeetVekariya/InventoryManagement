28e1b008db91375567e7e36f3c907524ff5d1da6
Heet Vekariya <heetvekariya50@gmail.com>
2025-03-13 17:06:27 +0530

    feat: create necessary components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => {
+            return import('./components/landing-page/landing-page.component').then(c => c.LandingPageComponent);
+        }
+    },
+    {
+        path: 'home',
+        loadComponent: () => {
+            return import('./components/home/home.component').then(c => c.HomeComponent);
+        }
+    },
+    {
+        path: 'categories',
+        loadComponent: () => {
+            return import('./components/category-list/category-list.component').then(c => c.CategoryListComponent);
+        }
+    },
+    {
+        path: 'items',
+        loadComponent: () => {
+            return import('./components/item-list/item-list.component').then(c => c.ItemListComponent);
+        }
+    },
+    {
+        path: 'sales',
+        loadComponent: () => {
+            return import('./components/sales-list/sales-list.component').then(c => c.SalesListComponent);
+        }
+    },
+    {
+        path: '**',
+        loadComponent: () => {
+            return import('./components/not-found/not-found.component').then(c => c.NotFoundComponent);
+        }
+    },
+
+];
