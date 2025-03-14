f6cdf22cbd2dae696c7b9757ae182c363379bfa0
Heet Vekariya <heetvekariya50@gmail.com>
2025-03-14 15:30:25 +0530

    feat: retrieve items data

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ItemService } from '../../service/item.service';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-item-list',
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.css'
+  styleUrl: './item-list.component.css',
 })
 export class ItemListComponent {
+  itemService = inject(ItemService);
 
+  items$ = this.itemService.getItems();
 }
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, Subject, tap } from 'rxjs';
+import { item } from '../models/item';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
-  constructor() {}
+  http = inject(HttpClient);
+  itemsSubject = new Subject<item>();
+  itemsObject = this.itemsSubject.asObservable();
+
+  getItems(): Observable<item[]> {
+    return this.http
+      .get<Array<item>>(`${environment.apiUrl}/items`)
+      .pipe(tap((items) => console.log(items)));
+  }
 }
