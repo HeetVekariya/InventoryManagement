6520d1a1965c4e36a862a864c158f7a448a3f9f7
Heet Vekariya <heetvekariya50@gmail.com>
2025-03-14 14:34:37 +0530

    style: add semicolon at the end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,52 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => {
-            return import('./components/landing-page/landing-page.component').then(c => c.LandingPageComponent);
-        }
+  {
+    path: '',
+    loadComponent: () => {
+      return import('./components/landing-page/landing-page.component').then(
+        (c) => c.LandingPageComponent
+      );
     },
-    {
-        path: 'home',
-        loadComponent: () => {
-            return import('./components/home/home.component').then(c => c.HomeComponent);
-        }
+  },
+  {
+    path: 'home',
+    loadComponent: () => {
+      return import('./components/home/home.component').then(
+        (c) => c.HomeComponent
+      );
     },
-    {
-        path: 'categories',
-        loadComponent: () => {
-            return import('./components/category-list/category-list.component').then(c => c.CategoryListComponent);
-        }
+  },
+  {
+    path: 'categories',
+    loadComponent: () => {
+      return import('./components/category-list/category-list.component').then(
+        (c) => c.CategoryListComponent
+      );
     },
-    {
-        path: 'items',
-        loadComponent: () => {
-            return import('./components/item-list/item-list.component').then(c => c.ItemListComponent);
-        }
+  },
+  {
+    path: 'items',
+    loadComponent: () => {
+      return import('./components/item-list/item-list.component').then(
+        (c) => c.ItemListComponent
+      );
     },
-    {
-        path: 'sales',
-        loadComponent: () => {
-            return import('./components/sales-list/sales-list.component').then(c => c.SalesListComponent);
-        }
+  },
+  {
+    path: 'sales',
+    loadComponent: () => {
+      return import('./components/sales-list/sales-list.component').then(
+        (c) => c.SalesListComponent
+      );
     },
-    {
-        path: '**',
-        loadComponent: () => {
-            return import('./components/not-found/not-found.component').then(c => c.NotFoundComponent);
-        }
+  },
+  {
+    path: '**',
+    loadComponent: () => {
+      return import('./components/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      );
     },
-
+  },
 ];
