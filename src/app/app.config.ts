3a293a5f4eead2d52beba0f730880c161da62eaf
Heet Vekariya <heetvekariya50@gmail.com>
2025-03-25 11:37:49 +0530

    feat: toast service functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,8 @@ import {
 } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +23,16 @@ export const appConfig: ApplicationConfig = {
         redirect_uri:
           typeof window !== 'undefined' ? window.location.origin : '',
       },
+    }),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 2500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      maxOpened: 3,
+      resetTimeoutOnDuplicate: true,
+      closeButton: true,
     }),
   ],
 };
